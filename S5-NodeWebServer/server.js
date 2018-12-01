5d533a179368ba7930d014f06f39e3119cb13b3c
Heinz Araque <araqueheinz@gmail.com>
2018-12-01 13:17:29 -0500

    add start to the script, Setup heroku port

--- S5-NodeWebServer/server.js
+++ S5-NodeWebServer/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 let app = express();
 //hbs partials 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -75,6 +77,6 @@ app.get('/bad', (req, res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log('server is up and running');
+app.listen(port,()=>{
+    console.log(`server is up and running on ${port} `);
 });
