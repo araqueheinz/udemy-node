1e9fa991c8fea6e5fe3b5fc1401960c76e761e08
Heinz Araque <araqueheinz@gmail.com>
2018-12-06 18:46:44 -0500

    add GET Todos/:id

--- S7-MongoDB-Mongoose/server/server.js
+++ S7-MongoDB-Mongoose/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require("./db/mongoose");
 let {Todo} = require("./models/todo");
@@ -25,6 +26,25 @@ app.get('/todos',(req, res)=>{
     },(err)=>{
         res.status(400).send(err);
     });
+});
+
+//GET /todos/1234
+app.get('/todos/:id', (req, res)=>{
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo || todo.length === 0){
+            return res.status(404).send()
+        }
+        res.send({todo})
+
+    }).catch((e)=>{
+        res.status(404).send();
+    });
+
 });
 
 app.listen(3004, () => {
